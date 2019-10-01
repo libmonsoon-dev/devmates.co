15471f56689a68893f223fbd125f26e3f24cdb53
Daniil <daniil.stepanenko.96@gamil.com>
2019-10-01 10:50:26 +0300

    27-09-2019

--- 27-09-2019/solution.go
+++ 27-09-2019/solution.go
@@ -1,1 +1,52 @@
 package solution
+
+import "image"
+
+func Solution(matrix [][]int) int {
+	isValidSquare := func(upperLeft image.Point, size int) bool {
+		for x := upperLeft.X; x < upperLeft.X+size; x++ {
+			for y := upperLeft.Y; y < upperLeft.Y+size; y++ {
+				if matrix[x][y] == 0 {
+					return false
+				}
+			}
+		}
+		return true
+	}
+	maxSideSize := 0
+
+	xLen := len(matrix)
+	for x := 0; x < xLen; x++ {
+		yLen := len(matrix[x])
+		for y := 0; y < yLen; y++ {
+			if matrix[x][y] == 1 {
+				valid := true
+				currentSize := 1
+
+				if currentSize > maxSideSize {
+					maxSideSize = currentSize
+				}
+
+				newPtr := image.Pt(x, y)
+
+				for valid {
+					currentSize++
+					newPtr.X += currentSize
+					newPtr.Y += currentSize
+
+					if newPtr.X <= xLen && newPtr.Y <= yLen {
+						valid = isValidSquare(image.Pt(x, y), currentSize)
+
+						if valid && currentSize > maxSideSize {
+							maxSideSize = currentSize
+						}
+					} else {
+						valid = false
+					}
+				}
+			}
+		}
+	}
+
+	return maxSideSize * maxSideSize
+}
--- 27-09-2019/solution_test.go
+++ 27-09-2019/solution_test.go
@@ -1,1 +1,21 @@
 package solution
+
+import "testing"
+
+func TestSolution(t *testing.T) {
+	test := func(matrix [][]int, expect int) {
+		if actual := Solution(matrix); expect != actual {
+			t.Errorf("expect != actual (%#v != %#v)", expect, actual)
+		}
+	}
+
+	test(
+		[][]int{
+			{1, 0, 1, 0, 0},
+			{1, 0, 1, 1, 1},
+			{1, 1, 1, 1, 1},
+			{1, 0, 0, 1, 0},
+		},
+		4,
+	)
+}
